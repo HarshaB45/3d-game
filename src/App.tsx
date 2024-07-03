ba29338f3063eb3237df866c04650e8f93d2f4d0
Harsha <hbiruduraju@gmail.com>
2024-07-03 10:59:04 +0530

    InitialPush

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(1);
 
-  return <div className="App"></div>;
+  return <div className="App">Hello world</div>;
 }
 
 export default App;
